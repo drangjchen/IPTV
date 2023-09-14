0d63c45dad5021d199fe4c422b571a936e260319
drangjchen <120029195+drangjchen@users.noreply.github.com>
2023-09-15 03:47:25 +0800

    Update replace_links.py

--- replace_links.py
+++ replace_links.py
@@ -1,19 +1,31 @@
 def replace_links(target_file):
-    # ... 其他代码 ...
+    with open(target_file, 'r') as file:
+        lines = file.readlines()
+
+    # 初始化一个空列表以存储新的链接
+    new_links = []
+
     replaced = False
 
     for index, line in enumerate(lines):
         if line.startswith('#EXTINF'):
             if not replaced:
                 # 替换链接为你想要的链接
-                lines[index + 1] = "https://raw.githubusercontent.com/YueChan/Live/main/IPTV.m3u\n"  # 替换为第一个链接
-                lines[index + 2] = "https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u\n"  # 替换为第二个链接
-                lines[index + 3] = "https://raw.githubusercontent.com/YanG-1989/m3u/main/Gather.m3u\n"  # 替换为第三个链接
+                new_links.append("https://raw.githubusercontent.com/YueChan/Live/main/IPTV.m3u\n")  # 替换为第一个链接
+                new_links.append("https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u\n")  # 替换为第二个链接
+                new_links.append("https://raw.githubusercontent.com/YanG-1989/m3u/main/Gather.m3u\n")  # 替换为第三个链接
                 replaced = True
 
+    # 将新的链接写入文件
     with open(target_file, 'w') as file:
-        file.writelines(lines)
+        for line in lines:
+            if line.startswith('#EXTINF'):
+                file.write(line)
+                for new_link in new_links:
+                    file.write(new_link)
+            else:
+                file.write(line)
 
 if __name__ == '__main__':
-    target_file = "M3U/YueChan_IPV6.m3u"
+    target_file = "./M3U/YueChan_IPV6.m3u"
     replace_links(target_file)
